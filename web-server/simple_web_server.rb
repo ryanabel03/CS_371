cc917d4bdd44eb7fe84feb66b86e14142c0a0fe1
Ryan Abel <abelr@mail.gvsu.edu>
2013-03-18 17:06:06 -0400

    Added lab 8.

--- web-server/simple_web_server.rb
+++ web-server/simple_web_server.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 
 get '/' do
- send_file 'labs/lab_4/index.php'
+ send_file 'labs/lab_8/index.html'
+end
+
+get '/jquery-1.9.1.min.js' do
+ send_file 'labs/lab_8/jquery-1.9.1.min.js'
 end
 
